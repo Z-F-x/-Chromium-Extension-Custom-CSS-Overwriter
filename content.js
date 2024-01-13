1b316953bd140a6eff6020c972f008e79bf3f532
benjamin-roedstien <benjamin.roedstien@gmail.com>
2024-01-13 16:14:12 +0100

    Update

--- content.js
+++ content.js
@@ -25,87 +25,94 @@ input[type="checkbox"], input[type="text"], input {
   color: #fff6ef!important;
 }
 
-  /* Add your custom CSS here */
+/* Add your custom CSS here */
 
 
 
+
+/* VG, Dagbladet, Aftenposten: */
+
+.horizontal-scroller, html, body {
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+span, h3,h1, h2{ color: #fff6ef!important;}
+.content-main-container{
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+:root{    background-color: #262729!important;}
+.ch-core-header .ch-header-main {
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+.ch-core-header .ch-logo:visited, .ch-icon ch-icon-logo, .ch-core-header .ch-main-links a, .ch-core-header .ch-main-links a:link{
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+
+.article-entity, .header, header, .carousel-see-more-livefeed, .teaser, children __sc, .block one-plus-three, .text-container, .information comments, .illustration-container, .vev, .col-2b, .article, .teaser-link, .content-outer, .content-container, .content _center, .page-main-container{
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+.teaser-link{
+  border-bottom: 1px solid gray;
+}
+
+::before {
+  background-color: #262729!important;
+}
+
+/* Etymology online: */
+.container--1mazc{
+  background-color: #262729!important;
+  color: #fff6ef!important;
+}
+
+/* translate.google.com: */
+
+/* chat.openai.com*/
+.sticky{
+  background-color: #262729!important;
+  color: #fff6ef!important;
+} 
+
+.p-4 .overflow-y-auto{
+  background-color: #222222!important;
+}
+.from-black{
+  // background-color: #222222!important;
+}
+
+.w-full text-token-text-primary, .dark .bg-black, .text-gizmo-gray-600 {
+  background-color: #333333!important;
+}
+
+.px-4, .py-2, .justify-center, .text-base, .md:gap-6, .m-auto, .flex, .flex-1, .text-base, .mx-auto, .gap-3, .md:px-5, .lg:px-1, .xl:px-5, .md:max-w-3xl, .lg:max-w-[40rem], .xl:max-w-[48rem], .group, .final-completion, .relative, .flex, .w-full, .flex-col, .lg:w-[calc(100%-115px)], .agent-turn{
+  background-color: #333333!important;
+}
+
+.to-black{
   
-  /* VG, Dagbladet, Aftenposten: */
+  // background-color: #333333!important;
+  // --tw-gradient-from-position: #333333!important;
+  // --tw-gradient-from: #333333!important var(#333333!important);
+  // --tw-gradient-to: #333333!important var(#333333!important);
+}
 
-  .horizontal-scroller, html, body {
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
-  span, h3,h1, h2{ color: #fff6ef!important;}
-  .content-main-container{
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
-  :root{    background-color: #262729!important;}
-  .ch-core-header .ch-header-main {
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
-  .ch-core-header .ch-logo:visited, .ch-icon ch-icon-logo, .ch-core-header .ch-main-links a, .ch-core-header .ch-main-links a:link{
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
+.flex items-center .justify-center .text-token-text-primary .transition hover:text-token-text-secondary .radix-state-open:text-token-text-secondary{
+  // background-color: #333333!important;
+  data-state: open;
+}
+//.group, w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-gray-700 dark:text-gray-300 md:whitespace-normal 
+.btn, .relative, .btn-neutral, .flex .flex-col .gap-2{
+  background-color: #333333!important;
+}
 
-  .article-entity, .header, header, .carousel-see-more-livefeed, .teaser, children __sc, .block one-plus-three, .text-container, .information comments, .illustration-container, .vev, .col-2b, .article, .teaser-link, .content-outer, .content-container, .content _center, .page-main-container{
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
-  .teaser-link{
-    border-bottom: 1px solid gray;
-  }
-
-  ::before {
-    background-color: #262729!important;
-  }
-
-
-
-  /* Etymology online: */
-  .container--1mazc{
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  }
-
-  /* translate.google.com: */
-
-  /* chat.openai.com*/
-  .sticky{
-    background-color: #262729!important;
-    color: #fff6ef!important;
-  } 
-
-  .p-4 .overflow-y-auto{
-    background-color: #222222!important;
-  }
-  .from-black{
-    // background-color: #222222!important;
-  }
-
-  .w-full text-token-text-primary, .dark .bg-black, .text-gizmo-gray-600 {
-    background-color: #333333!important;
-  }
-
-  .px-4, .py-2, .justify-center, .text-base, .md:gap-6, .m-auto, .flex, .flex-1, .text-base, .mx-auto, .gap-3, .md:px-5, .lg:px-1, .xl:px-5, .md:max-w-3xl, .lg:max-w-[40rem], .xl:max-w-[48rem], .group, .final-completion, .relative, .flex, .w-full, .flex-col, .lg:w-[calc(100%-115px)], .agent-turn{
-    background-color: #333333!important;
-  }
-
-  .to-black{
-    
-    // background-color: #333333!important;
-    // --tw-gradient-from-position: #333333!important;
-    // --tw-gradient-from: #333333!important var(#333333!important);
-    // --tw-gradient-to: #333333!important var(#333333!important);
-  }
-
-  .flex items-center .justify-center .text-token-text-primary .transition hover:text-token-text-secondary .radix-state-open:text-token-text-secondary{
-    // background-color: #333333!important;
-    data-state: open;
-  }
+/*Instagram*/
+section{
+  background-color: #262729!important;
+}
 
 `;
 
